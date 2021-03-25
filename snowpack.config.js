4104e451baeecdfe3ecc7cb1ddbea285b2d909cf
Allan Mobley <allan.mobley.jr@mobsites.com>
2021-03-25 11:45:14 -0400

    add

---
+++ snowpack.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    src: { url: '/', resolve: true, static: false }
+  },
+  packageOptions: {
+    source: 'local'
+  },
+  devOptions: {},
+  buildOptions: {
+    out: 'dist'
+  },
+  optimize: {
+    target: 'es2020',
+    minify: true
+  },
+  plugins: []
+};
