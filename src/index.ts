4f1e87023266ab14c0c45a02e30b8b49c61993a5
Allan Mobley <allan.mobley.jr@mobsites.com>
2021-03-27 14:43:50 -0400

    Added default export for Styled-Components

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { default as React } from 'react';
 export * from 'react-dom';
 export * from 'dexie';
 export * from 'dexie-react-hooks';
+export { default as styled } from 'styled-components';
 export * from 'styled-components';
